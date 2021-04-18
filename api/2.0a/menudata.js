9acb0a4a4ed2601a80276b8b05335a3b44458fba
Joseph Malloch <jmalloch@dal.ca>
2021-04-18 20:32:14 -0300

    Updated API documentation.

--- api/2.0a/menudata.js
+++ api/2.0a/menudata.js
@@ -32,10 +32,12 @@ var menudata={children:[
 {text:"All",url:"functions.html",children:[
 {text:"a",url:"functions.html#index_a"},
 {text:"d",url:"functions.html#index_d"},
+{text:"e",url:"functions.html#index_e"},
 {text:"f",url:"functions.html#index_f"},
 {text:"g",url:"functions.html#index_g"},
 {text:"i",url:"functions.html#index_i"},
 {text:"j",url:"functions.html#index_j"},
+{text:"l",url:"functions.html#index_l"},
 {text:"m",url:"functions.html#index_m"},
 {text:"n",url:"functions.html#index_n"},
 {text:"o",url:"functions.html#index_o"},
@@ -58,4 +60,5 @@ var menudata={children:[
 {text:"r",url:"functions_func.html#index_r"},
 {text:"s",url:"functions_func.html#index_s"},
 {text:"t",url:"functions_func.html#index_t"},
-{text:"u",url:"functions_func.html#index_u"}]}]}]}]}
+{text:"u",url:"functions_func.html#index_u"}]},
+{text:"Enumerations",url:"functions_enum.html"}]}]}]}
